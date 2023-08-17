047c6829bbf188577c73411423745c0785e0d562
Sean Cooper <seancooperwd@gmail.com>
2023-08-17 11:37:28 -0700

    fix apost build error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -24,7 +24,7 @@ function page() {
             How MILO Works
           </h2>
           <p className="text-xl text-white font-lato">
-            Every time you complete a workout, all you need to do is input how you felt during the workout, which we call <span className="text-lightBlue">Effort Level</span>, and note the start and end times of your workout. MILO's intelligent algorithm takes care of the rest, providing you with personalized insights and recommendations to help you achieve your fitness goals.
+            Every time you complete a workout, all you need to do is input how you felt during the workout, which we call <span className="text-lightBlue">Effort Level</span>, and note the start and end times of your workout. MILO&apos;s intelligent algorithm takes care of the rest, providing you with personalized insights and recommendations to help you achieve your fitness goals.
           </p>
         </div>
       </div>
@@ -35,7 +35,7 @@ function page() {
             What insights does MILO provide?
           </h2>
           <p className="text-xl text-white font-lato">
-            MILO's magic lies in the information it provides based on your workout data. You will discover your <span className="text-lightBlue">Fit Load</span>, a measure of your current training load, and your <span className="text-lightBlue">Fit Capacity</span>, a reflection of your long-term training adaptation. From these, your <span className="text-lightBlue">Performance Index</span> is calculated, helping identify the optimal balance for your fitness journey.
+            MILO&apos;s magic lies in the information it provides based on your workout data. You will discover your <span className="text-lightBlue">Fit Load</span>, a measure of your current training load, and your <span className="text-lightBlue">Fit Capacity</span>, a reflection of your long-term training adaptation. From these, your <span className="text-lightBlue">Performance Index</span> is calculated, helping identify the optimal balance for your fitness journey.
           </p>
         </div>
       </div>
@@ -46,7 +46,7 @@ function page() {
             How do these insights help me on my fitness journey?
           </h2>
           <p className="text-xl text-white font-lato">
-            MILO goes beyond mere numbers. Based off of your data, MILO places you in specific  <span className="text-lightBlue">Zones</span>: <span className="text-green">Undertraining</span>, <span className="text-cyan">Maintaining</span>, <span className="text-lightBlue">Optimal</span>, <span className="text-orange">Overreaching</span>, and <span className="text-red">Overtraining</span>. Each of these coming with recommendations on how to adjust your training to reach your goals. Whether you are aiming to optimize your performance, maintain your gains, or avoid overtraining— MILO's user-friendly system helps you navigate your fitness path with confidence.
+            MILO goes beyond mere numbers. Based off of your data, MILO places you in specific  <span className="text-lightBlue">Zones</span>: <span className="text-green">Undertraining</span>, <span className="text-cyan">Maintaining</span>, <span className="text-lightBlue">Optimal</span>, <span className="text-orange">Overreaching</span>, and <span className="text-red">Overtraining</span>. Each of these coming with recommendations on how to adjust your training to reach your goals. Whether you are aiming to optimize your performance, maintain your gains, or avoid overtraining— MILO&apos;s user-friendly system helps you navigate your fitness path with confidence.
           </p>
         </div>
       </div>
@@ -57,7 +57,7 @@ function page() {
             MILO can tell all that just from my Effort Level and Workout Duration?
           </h2>
           <p className="text-xl text-white font-lato">
-          Absolutely! MILO's ability to derive meaningful insights from your Effort Level and workout duration is firmly grounded in sports science. Research has demonstrated the scientific validity of Rating of Perceived Exertion (RPE) as a robust and intuitive measure of exercise intensity. By incorporating your Effort Level, MILO taps into your body's internal cues, providing a holistic view of how each workout is impacting your body. When combined with workout duration, MILO creates a comprehensive training load calculation that aligns with established principles of exercise physiology. This amalgamation of RPE and duration forms the bedrock of MILO's accurate and personalized fitness assessments, paving the way for a smarter, more effective fitness journey.
+          Absolutely! MILO&apos;s ability to derive meaningful insights from your Effort Level and workout duration is firmly grounded in sports science. Research has demonstrated the scientific validity of Rating of Perceived Exertion (RPE) as a robust and intuitive measure of exercise intensity. By incorporating your Effort Level, MILO taps into your body's internal cues, providing a holistic view of how each workout is impacting your body. When combined with workout duration, MILO creates a comprehensive training load calculation that aligns with established principles of exercise physiology. This amalgamation of RPE and duration forms the bedrock of MILO&apos;s accurate and personalized fitness assessments, paving the way for a smarter, more effective fitness journey.
           </p>
         </div>
       </div>
